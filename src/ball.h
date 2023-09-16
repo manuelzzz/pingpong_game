15e52665e02c2b991c3fe95e781c9287982a5479
Manel <manuelss.23.bil@gmail.com>
2023-09-16 12:16:00 -0300

    Class ball from .cpp for .h with pragma once

--- src/ball.h
+++ src/ball.h
@@ -1,27 +1,27 @@
+#pragma once
 #include <iostream>
 
 using namespace std;
 
-enum eDirection
-{
-    STOP = 0,
-    LEFT = 1,
-    UPLEFT = 2,
-    DOWNLEFT = 3,
-    RIGHT = 4,
-    UPRIGHT = 5,
-    DOWNRIGHT = 6,
-};
-
-class cBall
+class Ball
 {
 private:
     int x, y;
     int originalX, originalY;
+    enum eDirection
+    {
+        STOP = 0,
+        LEFT = 1,
+        UPLEFT = 2,
+        DOWNLEFT = 3,
+        RIGHT = 4,
+        UPRIGHT = 5,
+        DOWNRIGHT = 6,
+    };
     eDirection direction;
 
 public:
-    cBall(int posX, int poxY)
+    Ball(int posX, int poxY)
     {
         originalX = posX;
         originalY = poxY;
@@ -80,7 +80,7 @@ public:
         }
     }
 
-    friend ostream &operator<<(ostream &o, cBall c)
+    friend ostream &operator<<(ostream &o, Ball c)
     {
         o << "Ball[" << c.x << ", " << c.y << "][" << c.direction << "]" << endl;
         return o;
