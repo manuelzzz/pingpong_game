2fcf408540fceef38a546780efe876f61669a296
Manel <manuelss.23.bil@gmail.com>
2023-09-16 12:17:06 -0300

    Main class running draw function from gameManager

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,12 @@
 #include <iostream>
-#include "ball.cpp"
-#include "paddle.cpp"
+#include "gameManager.h"
 
 using namespace std;
 
 int main()
 {
-    cPaddle p1(0, 0), p2(10, 0);
-    cout << p1 << endl;
-    cout << p2 << endl;
-    p1.moveUp();
-    p2.moveDown();
-    cout << p1 << endl;
-    cout << p2 << endl;
+    GameManager game(40, 20);
+    game.Draw();
 
     return 0;
 }
